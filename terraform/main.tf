c9434e2daf30e0224115ee07c2c08aa4f37e25c1
Alex Gregory <alex.gregory@made.com>
2022-10-02 15:42:53 +0100

    test creating new project

---
+++ terraform/main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.38.0"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+resource "google_project" "my_project" {
+  name = "my_project_with_tf"
+  project_id = "alexgregory-created-with-tf"
+}
