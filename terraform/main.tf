30a8f96844b3476fef17ffb4f028115296dc69b5
Alex Gregory <alex.gregory@made.com>
2022-10-09 23:41:23 +0100

    add remote backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,13 @@
 terraform {
+
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "alexgregory"
+
+    workspaces {
+      name = "project-manager"
+    }
+  }
   required_providers {
     google = {
       source  = "hashicorp/google"
